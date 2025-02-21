05edce672e4526616611bbc0a486a6f4dfb8c168
Alexander Tejedor <atromero.98@gmail.com>
2025-02-21 18:52:41 -0500

    feat(App/index.jsx): I add a new funtion by routes the differents pages in a one page (App), creating a function called AppRoutes and add react-router-dom

--- src/pages/App/index.jsx
+++ src/pages/App/index.jsx
@@ -1,23 +1,30 @@
+import { useRoutes, BrowserRouter } from 'react-router-dom'
 import { Home } from '../Home'
 import { MyAccount } from '../MyAccount'
 import { MyOrder } from '../MyOrder'
 import { MyOrders } from '../MyOrders'
 import { NotFound } from '../NotFound'
 import { SignIn } from '../SignIn'
-
 import './App.css'
 
-function App() {
+const AppRoutes = () => {
+  let routes = useRoutes([
+    { path: '/', /* default route for the app*/ element: <Home /> /* component to render when the path is matched */},
+    { path: 'my-account', element: <MyAccount /> },
+    { path: 'my-order', element: <MyOrder /> },
+    { path: 'my-orders', element: <MyOrders /> },
+    { path: 'sign-in', element: <SignIn /> },
+    { path: '/*', element: <NotFound /> },
+])
+  return routes
+}
+
+const App = () => {
 
   return (
-    <div className="bg-amber-600">
-      <Home />
-      <MyAccount />
-      <MyOrder />
-      <MyOrders />
-      <NotFound />
-      <SignIn />
-    </div>
+    <BrowserRouter>
+      <AppRoutes />
+    </BrowserRouter>
   )
 }
 
