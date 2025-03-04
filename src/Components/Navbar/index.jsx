e02c4b3ccb7d43b7c406ee0a6ad5bfd34df1f4c3
Alexander Tejedor <atromero.98@gmail.com>
2025-03-03 21:55:09 -0500

    Creamos hicimos cambios en el Navbar para agregar las listas necesarias, y también importamos en el index de App para poder verlo en pantalla

--- src/Components/Navbar/index.jsx
+++ src/Components/Navbar/index.jsx
@@ -3,9 +3,20 @@ const Navbar = () => {
     return (
         <nav>
             <ul>
-                <li>
-                    <NavLink to='/'>Shopy</NavLink>
-                </li>
+                <li><NavLink to='/'>Shopy</NavLink></li>
+                <li><NavLink to='/'>All</NavLink></li>
+                <li><NavLink to='/clothes'>Clothes</NavLink></li>
+                <li><NavLink to='/electronics'>Electronics</NavLink></li>
+                <li><NavLink to='/fornitures'>Fornitures</NavLink></li>
+                <li><NavLink to='/toys'>Toys</NavLink></li>
+                <li><NavLink to='/others'>Others</NavLink></li>
+            </ul>
+            <ul>
+                <li>atromero.98@gmail.com</li>
+                <li><NavLink to='/my-orders'>My Orders</NavLink></li>
+                <li><NavLink to='/my-account'>My Acount</NavLink></li>
+                <li><NavLink to='/sign-in'>Sign In</NavLink></li>
+                <li>🛒 0</li>
             </ul>
         </nav>
     )
