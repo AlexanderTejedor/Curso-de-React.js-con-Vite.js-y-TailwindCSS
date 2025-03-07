e20e0120d8aa49e3c053f94cdfa40c44956a7c7d
Alexander Tejedor <atromero.98@gmail.com>
2025-03-07 18:26:55 -0500

    Finalizamos la estructura inicial de nuestro contexto global

---
+++ src/Context/index.jsx
@@ -0,0 +1,15 @@
+import { createContext } from 'react'
+import PropTypes from "prop-types"
+
+const ShoppingCartContext = createContext()
+
+export const ShoppingCartProvider = ({children}) => {
+    ShoppingCartProvider.propTypes = {
+            children: PropTypes.node.isRequired,
+        }
+    return (
+        <ShoppingCartContext.Provider>
+            {children}
+        </ShoppingCartContext.Provider>
+    )
+}
