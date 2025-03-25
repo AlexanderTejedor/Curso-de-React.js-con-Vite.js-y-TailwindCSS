8b5d27f7a2c24557877f135b5ca10aa79b5c72ab
Alexander Tejedor <atromero.98@gmail.com>
2025-03-25 07:14:55 -0500

    Modificamos el CheckoutSideMenu para agregar los elementos a este componenete, y también creamos el componente de OrderCard para mostrar estos elementos

--- src/Components/CheckoutSideMenu/index.jsx
+++ src/Components/CheckoutSideMenu/index.jsx
@@ -1,6 +1,7 @@
 import { useContext} from 'react'
 import { XMarkIcon } from "@heroicons/react/24/solid"
 import { ShoppingCartContext } from "../../Context";
+import { OrderCard } from '../OrderCard';
 import "./styles.css";
 
 const CheckoutSideMenu = () => {
@@ -10,6 +11,20 @@ const CheckoutSideMenu = () => {
             <div className="flex justify-between items-center p-6">
                 <h2 className="font-medium text-xl">My Order</h2>
                 <div><XMarkIcon className="size-6 text-black cursor-pointer" onClick={() => context.closeCheckoutSideMenu()}/></div>
+            </div>
+            <div className='px-6'>
+                {
+                    context.cartProducts.map(product => {
+                        return (
+                            <OrderCard
+                                key={product.id}
+                                title={product.title}
+                                imagerUrl={product.images}
+                                price={product.price}
+                            />
+                        )
+                    })
+                }
             </div>
         </aside>
     );
