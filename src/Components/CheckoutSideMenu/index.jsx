04baf93ece488bce29c7cd7103ea4c00f484cde4
Alexander Tejedor <atromero.98@gmail.com>
2025-04-02 06:33:33 -0500

    Finalizada la clase 21, en la que se vio como agregar el nuevo estado de limpiar el chekoutSideMenu, y se agrego el botón de Checkout

--- src/Components/CheckoutSideMenu/index.jsx
+++ src/Components/CheckoutSideMenu/index.jsx
@@ -11,13 +11,24 @@ const CheckoutSideMenu = () => {
         const filteredProducts = context.cartProducts.filter(product => product.id != id)
         context.setCartProducts(filteredProducts)
     }
+
+    const handleCheckout = () => {
+        const orderToAdd = {
+            date: '01.02.2025',
+            product: context.cartProducts,
+            totalproducts: context.cartProducts.length,
+            totalPrice: totalPrice(context.cartProducts)
+        }
+        context.setOrder([...context.order, orderToAdd])
+        context.setCartProducts([])
+    }
     return (
         <aside className={`${ context.isCheckoutSideMenuOpen ? 'flex' : 'hidden' } checkout-side-menu flex flex-col fixed right-0 border border-black rounded-lg bg-white`}>
             <div className="flex justify-between items-center p-6">
                 <h2 className="font-medium text-xl">My Order</h2>
                 <div><XMarkIcon className="size-6 text-black cursor-pointer" onClick={() => context.closeCheckoutSideMenu()}/></div>
             </div>
-            <div className='px-6 overflow-y-scroll'>
+            <div className='px-6 overflow-y-scroll flex-1'>
                 {
                     context.cartProducts.map(product => {
                         return (
@@ -33,11 +44,12 @@ const CheckoutSideMenu = () => {
                     })
                 }
             </div>
-            <div className='px-6'>
-                <p className='flex justify-between items-center'>
+            <div className='px-6 mb-6'>
+                <p className='flex justify-between items-center mb-2'>
                     <span className='font-light'>Total</span>
                     <span className='font-medium text-2xl'>${totalPrice(context.cartProducts)}</span>
                 </p>
+                <button className='w-full bg-black py-3 text-white rounded-lg cursor-pointer' onClick={() => handleCheckout()}>Checkout</button>
             </div>
         </aside>
     );
--- src/Context/index.jsx
+++ src/Context/index.jsx
@@ -23,6 +23,9 @@ export const ShoppingCartProvider = ({children}) => {
     // Shopping Cart - add product to cart
     const [cartProducts, setCartProducts] = useState([]);
 
+    // Shopping Cart - Order
+    const [order, setOrder] = useState([]);
+
     ShoppingCartProvider.propTypes = {
             children: PropTypes.node.isRequired,
         }
@@ -39,7 +42,9 @@ export const ShoppingCartProvider = ({children}) => {
             setCartProducts,
             isCheckoutSideMenuOpen,
             openCheckoutSideMenu,
-            closeCheckoutSideMenu
+            closeCheckoutSideMenu,
+            order,
+            setOrder
         }}>
             {children}
         </ShoppingCartContext.Provider>
